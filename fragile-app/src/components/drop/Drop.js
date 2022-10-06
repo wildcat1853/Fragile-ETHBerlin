5b05e8d3386b73b6c95920eae0753cf39a413262
Anton <anton.kov.hack@proton.me>
2022-10-06 19:40:52 +0200

    Slight refactor

--- fragile-app/src/components/drop/Drop.js
+++ fragile-app/src/components/drop/Drop.js
@@ -9,6 +9,7 @@ import NFTModal from '../modal/modal';
 import { useQuery } from '@apollo/client';
 import query from '../../service/graphQuery';
 import * as  utils from '../../service/utils';
+import { collection } from '../../collection/data';
 
 const Drop = () => {
   const [open, setOpen] = useState(false);
@@ -17,15 +18,15 @@ const Drop = () => {
     setOpen(true);
     setUrl(url);
   }
-  const { loading, error, data } = useQuery(query.fetchNFTInfo());
-  // TODO: Perform correct error and loading handling
-  if (loading) return <div>Loading...</div>
-  if (error) {
-    console.error(error);
-    return <div>Error...</div>
-  }
-  const {nodes, pageInfo} = data.tokens
-  const {endCursor} = pageInfo
+  // const { loading, error, data } = useQuery(query.fetchNFTInfo());
+  // // TODO: Perform correct error and loading handling
+  // if (loading) return <div>Loading...</div>
+  // if (error) {
+  //   console.error(error);
+  //   return <div>Error...</div>
+  // }
+  // const {nodes, pageInfo} = data.tokens
+  // const {endCursor} = pageInfo
   
   return (
     <Container sx={{marginTop:'100px'}}>
@@ -35,14 +36,16 @@ const Drop = () => {
       </Box>
       <Box sx={{display:'block', marginTop:10,}}>
         {
-          nodes.map(node => {
-            const {tokenId, metadata} = node.token
-            const imageUrl = utils.getIpfs(metadata.image) // Parse to correct ipdf format
+          Object.keys(collection).map(contract => {
+            // const {tokenId, metadata} = node.token
+            // const imageUrl = utils.getIpfs(metadata.image) // Parse to correct ipdf format
+            const tokenId = contract;
+            const imageUrl = utils.getIpfs(collection[contract]);
             
             return <NFTBadge 
-            key={tokenId}
-            url={imageUrl}
-            handleModalClick={() => handleModalClick(imageUrl)} 
+              key={tokenId}
+              url={imageUrl}
+              handleModalClick={() => handleModalClick(imageUrl)} 
             />
           })
         }
